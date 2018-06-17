9082d2d69b4022256345500eb4a25b357c042d8c
Hotanya <hotanya.r@gmail.com>
2018-06-17 19:55:58 +1200

    Compare generated checksum with a user provided one!

--- checksum.py
+++ checksum.py
@@ -1,14 +1,42 @@
 import sys
 import os
+import hashlib
 
-file_to_check = raw_input("Enter the path of the file to check:")
+file_to_check = input("Enter the path of the file to check:") 
 
-command = 'certutil -hashfile ' + file_to_check
+hash_alg = input("Which hash algorithm would you like to use? Options are: MD5, SHA1, SHA256, SHA512")
 
-hash_type = raw_input("Which Hash Algorithm do you want to check: HashAlgorithm choices: MD2 MD4 MD5 SHA1 SHA256 SHA384 SHA512 ")
-command = "certutil -hashfile " + file_to_check + " " + hash_type
+if (hash_alg == "MD5" or hash_alg == "md5"):
+    checksum = hashlib.md5(open('{0}'.format(file_to_check),'rb').read()).hexdigest() #opens the file as a hexdigest so the checksum will be of the file CONTENTS not filename
+    print(checksum)
 
-os.system('{0}'.format(command))
+elif (hash_alg == "SHA1" or hash_alg == "sha1"):
+    checksum = hashlib.sha1(open('{0}'.format(file_to_check),'rb').read()).hexdigest() 
+    print(checksum)  
+
+elif (hash_alg == "SHA256" or hash_alg == "sha256"):
+    checksum = hashlib.sha256(open('{0}'.format(file_to_check),'rb').read()).hexdigest() 
+    print(checksum)  
+
+elif (hash_alg == "SHA512" or hash_alg == "sha512"):
+    checksum = hashlib.sha512(open('{0}'.format(file_to_check),'rb').read()).hexdigest() 
+    print(checksum)        
+
+
+################################# Compare checksum code ###################################
+
+compare = input("Do you want to compare this checksum? y or n ")
+
+if (compare == 'y'):
+    checksum_to_compare = input("Please enter the checksum to compare against: ")   
+    
+    if (checksum_to_compare == checksum):
+        print("Congrats! They match!")
+    else:
+        print("Uh Oh, they don't match")
+
+else:
+    print("Have a nice day")
 
 
 # Hotanya Ragtah - 2018
